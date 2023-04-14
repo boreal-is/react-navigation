1c0d7e11c67fbdc9d74675d22aed06e58065d92e
abeausoleil <alexgoodsun@hotmail.com>
2023-04-14 11:19:53 -0400

    Ad Hoc - Fix compatibility with react-native

--- packages/native/src/withOrientation.js
+++ packages/native/src/withOrientation.js
@@ -14,11 +14,11 @@ export default function (WrappedComponent) {
     }
 
     componentDidMount() {
-      Dimensions.addEventListener('change', this.handleOrientationChange);
+      this.changeSubscription = Dimensions.addEventListener('change', this.handleOrientationChange);
     }
 
     componentWillUnmount() {
-      Dimensions.removeEventListener('change', this.handleOrientationChange);
+      this.changeSubscription.remove();
     }
 
     handleOrientationChange = ({ window }) => {
